733bcf6346010d68994d44a342c241c5e65bf95c
Jazz-w <jazzxbusiness@outlook.com>
2025-02-14 08:03:47 +0100

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,9 @@
 import { Inter , Kanit , Lusitana} from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] }); 
+export const kanit = Kanit( { weight: ['400', '700']
+    ,subsets :['latin'],
+});
 export const lusitana = Lusitana({
     weight: ['400', '700'],
     subsets: ['latin'],
